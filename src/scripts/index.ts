1aba39e04248cba6ceb86cd8d74db7339f8a17e8
Ben Littleton <b.little1996@gmail.com>
2021-08-14 21:21:53 -0400

    Fixed backout logic for rendering actor sheetets

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -4,15 +4,11 @@ Hooks.on(
     const { user } = game as Game;
     const { actor } = sheet;
 
-    if (!user || !actor.canUserModify(user, "update")) {
-      ui.notifications?.error(
-        "You must be the owner of a sheet to import a character."
-      );
-      return;
-    } else if (actor.data.type !== "character") {
-      ui.notifications?.error(
-        `You can only import a character into a character sheet, not a ${actor.data.type} sheet`
-      );
+    if (
+      actor.data.type !== "character" ||
+      !user ||
+      !actor.canUserModify(user, "update")
+    ) {
       return;
     }
   }

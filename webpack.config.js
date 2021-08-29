a47a12e3c7757172ddfb4df51b964a236d6a97a4
Ben Littleton <b.little1996@gmail.com>
2021-08-29 13:11:47 -0400

    Removed parcel, updated webpack config to allow for flexibility during development.

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,32 @@
 const path = require("path");
+const dotenv = require("dotenv");
 
-const config = {
-  entry: "./src/index.ts",
-  module: {
-    rules: [
-      {
-        test: /.tsx?$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  resolve: {
-    extensions: [".ts"],
-  },
-  output: {
-    filename: "module.js",
-    path: path.resolve(__dirname, "build"),
-  },
+dotenv.config();
+
+const config = (env, options) => {
+  const isDev = options.mode === "development";
+  const devDir = process.env.LOCAL_FOUNDRY_MODULE_PATH || __dirname;
+  const prodDir = process.env.PROD_BUILD_PATH || __dirname;
+
+  return {
+    entry: "./src/index.ts",
+    module: {
+      rules: [
+        {
+          test: /.tsx?$/,
+          use: "ts-loader",
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".ts"],
+    },
+    output: {
+      filename: "module.js",
+      path: path.resolve(isDev ? devDir : prodDir, "build"),
+    },
+  };
 };
 
 module.exports = config;
